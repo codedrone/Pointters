6d6cfc95c3963ac09a55176268957f4096792739
cereceres <jesus.edelcereceres@gmail.com>
2017-07-26 16:11:50 -0500

    the supertest is added

--- server/app.js
+++ server/app.js
@@ -4,15 +4,15 @@ const flash = require('express-flash');
 const config = require('./config')();
 
 const app = express();
-const server = require('http').createServer(app);
 
 
 app.use(flash());
 require('./config/express')(app);
 require('./lib/routes')(app);
 
+const server = require('http').createServer(app);
 server.listen(config.port, () => {
     console.info('Express server listening on %d', config.port);
 });
 
-module.exports = app;
+module.exports = server;
