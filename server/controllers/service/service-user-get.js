6796f3c634a83f8b5070bd01de7eb7fe4db31159
bstarr322 <jointstarsoft@gmail.com>
2017-11-01 14:03:48 -0300

    insert 401 error controllers/service/service-user-get.js

--- server/controllers/service/service-user-get.js
+++ server/controllers/service/service-user-get.js
@@ -3,14 +3,19 @@ const { find } = require('../../../stores/service');
 const errorMessage = 'Error in find service';
 
 module.exports = async(ctx) => {
-    let { userId } = ctx.query;
-    const { id } = ctx.session;
-    if (!userId) userId = id;
-    const services = await find({ userId });
-    if (!services || services.error) {
-    	ctx.status = 404;
-    	ctx.body = 'No service found';
+    if (ctx.session) {
+        let { userId } = ctx.query;
+        const { id } = ctx.session;
+        if (!userId) userId = id;
+        const services = await find({ userId });
+        if (!services || services.error) {
+            ctx.status = 404;
+            ctx.body = 'No service found';
+        }
+        ctx.status = 200;
+        ctx.body = { services };
+    } else {
+        ctx.status = 401;
+        ctx.body = 'No Authorization';
     }
-    ctx.status = 200;
-    ctx.body = { services };
 };
