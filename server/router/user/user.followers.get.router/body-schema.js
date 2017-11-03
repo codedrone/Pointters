62635c4e8c750050c8da51baf07adc443182be9e
bstarr322 <jointstarsoft@gmail.com>
2017-11-03 18:43:49 -0300

    add router in serevr

---
+++ server/router/user/user.followers.get.router/body-schema.js
@@ -0,0 +1,13 @@
+const joi = require('joi');
+
+const generalNotifications = [ 'pushNotification', 'email', 'all' ];
+const orderNotifications = [ 'pushNotification', 'email', 'all' ];
+const offerNotifications = [ 'pushNotification', 'email', 'all' ];
+const summaryEmail = [ 'daily', 'weekly', 'all' ];
+
+module.exports = joi.object().keys({
+    generalNotifications: joi.string().valid(generalNotifications),
+    orderNotifications: joi.string().valid(orderNotifications),
+    offerNotifications: joi.string().valid(offerNotifications),
+    summaryEmail: joi.string().valid(summaryEmail)
+});
