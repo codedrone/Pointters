7a3978e7bc2da0a3caee6edb350f8d7fdcc610cc
bstarr322 <jointstarsoft@gmail.com>
2017-11-08 12:59:46 -0400

    add models of like and watch

---
+++ stores/like/schema.js
@@ -0,0 +1,26 @@
+const {Schema} = require('mongoose');
+
+module.exports = {
+    userId : {
+        type:Schema.Types.ObjectId,
+        required: true,
+        index: true,
+        ref: 'user'
+    },
+    serviceId : {
+        type:Schema.Types.ObjectId,
+        required: true,
+        index: true,
+        ref: 'user'
+    },
+    updatedAt: {
+        type: Date,
+        default: new Date()
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+};
+
+
