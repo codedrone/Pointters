211bdb5f0ac7fe8c9bdb7c7ee06e1d693344c728
bstarr322 <jointstarsoft@gmail.com>
2017-11-10 11:45:09 -0400

    test for GET/services

--- test/big/service/get-services.test.js
+++ test/big/service/get-services.test.js
@@ -1,51 +1,112 @@
 const assert = require('assert');
-
-
+const faker = require('faker');
+const supertest = require('supertest');
+const app = require('../../../server');
+const { create: createUser } = require('../../../stores/user');
+const { create: createService } = require('../../../stores/service');
+const agent = supertest(app);
 describe('User services', () => {
     describe('SUCCESS', () => {
-        it('/services GET -> should return user services', async() => {
+        it('/services GET -> should return 200', async() => {
+            
             const body = {
-                email: 'test_get_settings@test.com',
-                password: 'test',
-                userId: require('mongoose').Types.ObjectId()
+                email: faker.internet.email(),
+                password: faker.internet.password()
             };
+            const user = await createUser(body);
+            const service = await createService({ userId: user._id, category: body, description: "test get-services" });
             const {
                 body: { token },
                 headers: { 'set-cookie': cookie }
-            } = await agent.post('/user/login').send({
+            } = await agent
+            .post('/user/login')
+            .send({
                 email: body.email,
                 password: body.password
             });
             const authorizationHeader = { Authorization: `Bearer ${token}` };
             const Cookie = { Cookie: cookie };
-            const { body: res } = await agent.get(`/services`)
+            const { body: { docs } } = await agent.get(`/services`)
                 .set(authorizationHeader)
                 .set(Cookie)
                 .expect(200);
-            assert(res.success === true);
-            assert(typeof res.services === 'array');
+            assert(typeof docs === 'array' || typeof docs === 'object');
         });
-    });
-    describe('FAIL', () => {
-        it('/services GET sohuld create a service given', async () => {
-            await agent.get('/services')
-                .set(authorizationHeader)
-                .set(Cookie)
-                .expect(404);
-        });
-    });
-    describe('FAIL', () => {
-        it('/services GET -> return error if pass is wrong', async() => {
+        it('/services/?userId=:id GET -> should return 200', async() => {
+            
             const body = {
                 email: faker.internet.email(),
                 password: faker.internet.password()
             };
-            await createUser(body);
-            body.password = faker.internet.password();
-            const { body: res } = await agent.post('/user/login')
-                .send(body)
+            const bodyOther = {
+                email: faker.internet.email(),
+                password: faker.internet.password()
+            };
+            const user = await createUser(body);
+            const userOther = await createUser(bodyOther);
+            const service = await createService({ userId: userOther._id, category: body, description: "test get-services/?userId" });
+            const {
+                body: { token },
+                headers: { 'set-cookie': cookie }
+            } = await agent
+                .post('/user/login')
+                .send(body);
+            console.log("authorizationHeader =", authorizationHeader);
+            const Cookie = { Cookie: cookie };
+            const { body: { docs } } = await agent.get(`/services/?userId=` + userOther._id)
+                .set(authorizationHeader)
+                .set(Cookie)
+                .expect(200);
+            assert(typeof docs === 'array' || typeof docs === 'object');
+        });
+        it('/services GET -> should return 404', async() => {
+            
+            const body = {
+                email: faker.internet.email(),
+                password: faker.internet.password()
+            };
+            const user = await createUser(body);
+            const {
+                body: { token },
+                headers: { 'set-cookie': cookie }
+            } = await agent
+            .post('/user/login')
+            .send({
+                email: body.email,
+                password: body.password
+            });
+            const authorizationHeader = { Authorization: `Bearer ${token}` };
+            const Cookie = { Cookie: cookie };
+            const { body: { message } } = await agent.get(`/services`)
+                .set(authorizationHeader)
+                .set(Cookie)
+                .expect(404);
+            assert(message === 'No service found');
+        });
+        it('/services GET -> should return 401', async() => {
+            
+            const body = {
+                email: faker.internet.email(),
+                password: faker.internet.password()
+            };
+            const user = await createUser(body);
+            const {
+                body: { token },
+                headers: { 'set-cookie': cookie }
+            } = await agent
+            .post('/user/login')
+            .send({
+                email: body.email,
+                password: body.password
+            });
+            console.log("token =======", token)
+            const authorizationHeader = { Authorization: `Bearer test` };
+            const Cookie = { Cookie: cookie };
+            const { body: { message } } = await agent.get(`/services`)
+                .set(authorizationHeader)
+                .set(Cookie)
                 .expect(401);
-            assert(res.message === 'Authentication failed.');
+            assert(message === 'Authentication Error');
         });
     });
 });
